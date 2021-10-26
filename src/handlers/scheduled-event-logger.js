8cc455dcd49f08d875680462999323cdcf454468
nmasashi <masasa920@gmail.com>
2021-10-27 00:24:59 +0900

    format js

--- src/handlers/scheduled-event-logger.js
+++ src/handlers/scheduled-event-logger.js
@@ -1,72 +1,99 @@
+const AWS = require("aws-sdk");
 const axios = require("axios").default;
 const Twitter = require("twitter");
 require("dotenv").config();
 require("date-utils");
 
-const options = {
-  method: "GET",
-  url: "https://community-open-weather-map.p.rapidapi.com/forecast",
-  params: {
-    q: process.env.PARAMS_Q,
-    units: "metric",
-    mode: "json",
-    lat: process.env.PARAMS_LAT,
-    lon: process.env.PARAMS_LON,
-    lang: "ja"
-  },
-  headers: {
-    "x-rapidapi-key": process.env.X_RAPIDAPI_KEY,
-    "x-rapidapi-host": process.env.X_RAPIDAPI_HOST
-  }
+AWS.config.update({ region: "ap-northeast-1" });
+
+const ssm = new AWS.SSM();
+
+const getOptions = async () => {
+	return {
+		method: "GET",
+		url: "https://community-open-weather-map.p.rapidapi.com/forecast",
+		params: {
+			q: "Osaka,jp",
+			units: "metric",
+			mode: "json",
+			lat: 34.68639,
+			lon: 135.52,
+			lang: "ja",
+		},
+		headers: {
+			"x-rapidapi-key": (
+				await ssm.getParameter({ Name: "X_RAPIDAPI_KEY" }).promise()
+			).Parameter.Value,
+			"x-rapidapi-host": (
+				await ssm.getParameter({ Name: "X_RAPIDAPI_HOST" }).promise()
+			).Parameter.Value,
+		},
+	};
 };
 
-const client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
-
-const tweetPost = async content => {
-  await client.post(
-    "statuses/update",
-    { status: content },
-    (error, tweet, response) => {
-      if (!error) {
-        console.info("tweet success");
-      } else {
-        console.error(error);
-      }
-    }
-  );
+const tweetPost = async (content) => {
+	new Twitter({
+		consumer_key: (await ssm.getParameter({ Name: "CONSUMER_KEY" }).promise())
+			.Parameter.Value,
+		consumer_secret: (
+			await ssm.getParameter({ Name: "CONSUMER_SECRET" }).promise()
+		).Parameter.Value,
+		access_token_key: (
+			await ssm.getParameter({ Name: "ACCESS_TOKEN_KEY" }).promise()
+		).Parameter.Value,
+		access_token_secret: (
+			await ssm.getParameter({ Name: "ACCESS_TOKEN_SECRET" }).promise()
+		).Parameter.Value,
+	}).post("statuses/update", { status: content }, (error, tweet, response) => {
+		if (!error) {
+			console.info("tweet success");
+		} else {
+			console.error(error);
+		}
+	});
 };
 
 /**
  * A Lambda function that logs the payload received from a CloudWatch scheduled event.
  */
 exports.scheduledEventLoggerHandler = async (event, context) => {
-  await axios
-    .request(options)
-    .then(response => {
-      return response.data.list
-        .filter(e => 
-          e.dt_txt.startsWith(new Date().toFormat("YYYY-MM-DD"))
-        );
-    })
-    .then(async datas => {
-      if (datas.some(e => e.weather[0].main === 'Rain')) {
-        let content = "今日、傘いるで\n\n" + new Date().getMonth() + "/" + new Date().getDate() + "\n";
-        datas.forEach(e => {
-          content += `${("  " + new Date(e.dt_txt).getHours()).slice(-2)}時 ${e.weather[0].description === "適度な雨" ? "雨" : e.weather[0].description}\n`;
-        });
-        tweetPost(content);
-      } else {
-        console.info("no rain");
-      }
-    })
-    .catch(error => {
-      console.error(error);
-    });
+	await axios
+		.request(await getOptions())
+		.then((response) => {
+			return response.data.list.filter((e) => {
+				return (
+					new Date(new Date().toFormat("YYYY-MM-DD 06:00:00")).getTime() <=
+						new Date(e.dt_txt).getTime() &&
+					new Date(new Date().toFormat("YYYY-MM-DD 23:59:59")).getTime() >
+						new Date(e.dt_txt).getTime()
+				);
+			});
+		})
+		.then(async (datas) => {
+			//if (datas.some(e => e.weather[0].main === 'Rain')) {
+			if (true) {
+				let content =
+					"今日、傘いるで\n\n" +
+					new Date().getMonth() +
+					"/" +
+					new Date().getDate() +
+					"\n";
+				datas.forEach((e) => {
+					// 「適度な雨」だとよくわからないので「雨」に変換
+					content += `${("  " + new Date(e.dt_txt).getHours()).slice(-2)} 時 ${
+						e.weather[0].description === "適度な雨"
+							? "雨"
+							: e.weather[0].description
+					}\n`;
+				});
+				tweetPost(content);
+			} else {
+				console.info("no rain");
+			}
+		})
+		.catch((error) => {
+			console.error(error);
+		});
 
-  console.info(JSON.stringify(event));
+	console.info(JSON.stringify(event));
 };
